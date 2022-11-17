604cc0df19e1f71c2273974474b2d17ea713a1f4
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2022-11-17 16:11:55 +0300

    checkLogin1

--- src/data/Main.java
+++ src/data/Main.java
@@ -3,7 +3,7 @@ package data;
 public class Main {
     public static void main(String[] args) throws WrongPasswordException {
         Data data = new Data("rtgfbnjkSltiopfdxsay",
-                "12345678901234567899",
+                "12345678901234567890",
                 "12345678901234567890");
         Data.test(data.getLogin(), data.getPassword(), data.getConfirmPassword());
     }
